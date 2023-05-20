1f614fbe92310508b7ba967715b55597324c9f9c
hasanozbakir <hasanozbakir@yahoo.com>
2023-05-20 19:28:49 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,19 @@
+const unitArray = [
+    {id: "length-section", 
+    firstType:"meters", 
+    secondType: "feet",
+    firstRatio: 3.281, 
+    secondRatio: 0.305},
+    {id: "volume-section", 
+    firstType:"liters", 
+    secondType: "gallons",
+    firstRatio: 0.264, 
+    secondRatio: 3.788},
+    {id: "mass-section", 
+    firstType:"kilos", 
+    secondType: "pounds",
+    firstRatio: 2.204, 
+    secondRatio: 0.454}
+]
+
+export default unitArray;
---
+++ index.js
@@ -0,0 +1,53 @@
+import unitArray from "./data.js"
+
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("btn-el");
+const app = document.querySelector("body");
+const toggleLabel = document.getElementById("toggle-checkbox")
+
+let unitValue = 0;
+let temporaryValue = 0;
+
+renderUnits();
+
+inputEl.addEventListener("input", function(e) {
+  e.preventDefault()
+  e.target.value = e.target.value.trim();
+  e.target.value = !isNaN(e.target.value) ? e.target.value : temporaryValue;
+  temporaryValue = e.target.value;
+  e.target.size = e.target.value.length > 2 ? e.target.value.length - 1 : 1;
+  unitValue = Number(e.target.value);
+});
+
+inputEl.addEventListener("keydown", function(e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    convertBtn.click();
+  }
+});
+
+convertBtn.addEventListener("click", renderUnits);
+
+function renderUnits(){
+  for(let unit of unitArray){
+    document.getElementById(unit.id).innerHTML = `
+          <span class="input-value">${unitValue}</span>
+            ${unit.firstType} =
+          <span id="mass-pounds-result">${(unitValue*unit.firstRatio).toFixed(2).replace(/[\.,]0{2}/, '')}</span>
+            ${unit.secondType} |
+          <span class="input-value">${unitValue}</span>
+            ${unit.secondType} =
+          <span id="mass-kilograms-result">${(unitValue*unit.secondRatio).toFixed(2).replace(/[\.,]0{2}/, '')}</span>
+            ${unit.firstType}`
+  }
+}
+
+toggleLabel.addEventListener("click", function(e) {
+  e.preventDefault()
+  if (app.getAttribute("light-mode") == "light") {
+      app.setAttribute("light-mode", "dark");
+  } else {
+      app.setAttribute("light-mode", "light");
+  }
+});
+
